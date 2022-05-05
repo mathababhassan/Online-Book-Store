55970cf875b0b092a8bb6cc308cde788704cc689
mathababhassan <103527892+mathababhassan@users.noreply.github.com>
2022-05-05 23:16:29 +0200

    Add files via upload

---
+++ create_user.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+	<link rel="stylesheet" href="../../style.css">
+</head>
+<body>
+<header>
+        <h1 class="logo">Bookan</h1>
+        <form action="#">
+            <label>Search</label>
+                <input type="text">
+        </form>
+
+        <nav>
+            <a href="../../cart.php">Cart</a>
+			<a href="../../home.php">Home</a>
+            <a href="../../books.php">Books</a>
+            <a href="../../login.php">Login</a>
+            <a href="../../contact.php">Contact Us</a>
+        </nav>
+
+        <h2>Admin - Create User</h2>
+    </header>
+
+    <main>
+        <form action="create_user.php">
+            <section class="input-group">
+				<label>Username</label>
+				<input type="text" name="username" value="<?php echo $username; ?>">
+			</section>
+            
+			<section class="input-group">
+				<label>Email</label>
+				<input type="email" name="email" value="<?php echo $email; ?>">
+			</section>
+
+			<section class="input-group">
+				<label>User type</label>
+				<select name="user_type" class="user_type" >
+					<option value=""></option>
+					<option value="admin">Admin</option>
+					<option value="user">User</option>
+				</select>
+			</section>
+
+			<section class="input-group">
+				<label>Password</label>
+				<input type="password" name="password_1">
+			</section>
+
+			<section class="input-group">
+				<label>Confirm password</label>
+				<input type="password" name="password_2">
+			</section>
+
+			<section class="input-group">
+				<button type="submit" class="btn" name="register_btn"> + Create user</button>
+			</section>
+        </form>
+
+		<?php require("../../footer.php") ?>
+    </main>
+</body>
+</html>
